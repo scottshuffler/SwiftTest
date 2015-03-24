5f11cad04d54fc2c87779d8f4317518d73239d80
Scott <Owner@Laptop.local>
2015-03-24 18:07:04 -0400

    Functional slider

--- helloWorld/ViewController.swift
+++ helloWorld/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     @IBOutlet var bfText: UITextField!
     @IBOutlet weak var slider: UISlider!
     var bodyFat : Int!
-    var height : Double!
+    var height : Int!
     var newweight : Double!
     var oldWeight : Int!
     var tempWeight : Double!
@@ -27,15 +27,41 @@ class ViewController: UIViewController {
     var tempDouble : Double!
     var final : String!
     var wasSlided : Bool = true
+    var bmr : Double!
+    var age : Int!
+    var slideVal : Int = 0
+    var bmi : Double!
 
+    @IBOutlet weak var bmrLab: UILabel!
     @IBOutlet weak var sexLabel: UILabel!
     @IBOutlet weak var toggle: UISwitch!
     @IBAction func valueChange(sender: UISlider) {
         var currentValue = Int(sender.value)
-        slideLabel.text = "\(currentValue)"
+        slideVal = Int(sender.value)
+        if (slideVal == 0)
+        {
+            slideLabel.text = "Sedentary"
+        }
+        else if (slideVal == 1)
+        {
+            slideLabel.text = "Lightly Active"
+        }
+        else if (slideVal == 2)
+        {
+            slideLabel.text = "Moderately Active"
+        }
+        else if (slideVal == 3)
+        {
+            slideLabel.text = "Very Active"
+        }
+        else
+        {
+            slideLabel.text = "Heavily Active"
+        }
     }
     
     @IBAction func sliderAction(sender: UISwitch) {
+        
         wasSlided = !wasSlided
         if (wasSlided)
         {
@@ -49,9 +75,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var slideLabel: UILabel!
     @IBAction func buttonPressed(sender: AnyObject) {
         println("You clicked the button")
-        height = atof(hText.text)
-        height = height * 2.54
+        height = hText.text.toInt()
         oldWeight = wText.text.toInt()
+        age = aText.text.toInt()
+        if (wasSlided)
+        {
+            bmr = 66+(6.23 * Double(oldWeight))+(12.7 * Double(height))-(6.8 * Double(age))
+        }
+        else
+        {
+            
+            bmr = 655 + (4.35 * Double(oldWeight)) + (4.7 * Double(height)) - (4.7 * Double(age))
+        }
+        if (slideVal == 0)
+        {
+            bmi = bmr * 1.2
+        }
+        else if (slideVal == 0)
+        {
+            bmi = bmr * 1.375
+        }
+        else if (slideVal == 0)
+        {
+            bmi = bmr * 1.55
+        }
+        else if (slideVal == 0)
+        {
+            bmi = bmr * 1.725
+        }
+        else
+        {
+            bmi = bmr * 1.9
+        }
+        tText.text = String(format:"%.1f", bmi)
+        bmrLab.text = String(format:"%.1f",bmr)
         println(wText.text)
         bodyFat = bfText.text.toInt()
         println(bfText.text)
@@ -59,7 +116,7 @@ class ViewController: UIViewController {
         tempCalc = Double(oldWeight)
         tempWeight = tempCalc * tempDouble
         newweight = tempCalc - tempWeight
-        tText.text = String(format:"%.1f", height)
+        
         lText.text = String(format:"%.1f", newweight)
     }
     override func viewDidLoad() {
